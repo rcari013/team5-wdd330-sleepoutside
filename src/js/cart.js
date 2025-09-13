7e0485654b51e6b72cb2bd304878ee1e010f8db0
rcari013 <carinorome@gmail.com>
2025-09-13 15:02:45 +0800

    Fix: display total on cart page

--- src/js/cart.js
+++ src/js/cart.js
@@ -7,23 +7,18 @@ function renderCartContents() {
 }
 
 function cartItemTemplate(item) {
-  // normalize product id
-  const productId = item.Id || item.id || item.SKU || item.sku;
-  const link = `../product_pages/index.html?product=${productId}`;
-
-  return `
-    <li class="cart-card divider">
-      <a href="${link}" class="cart-card__image">
-        <img src="${item.Image}" alt="${item.Name}" />
-      </a>
-      <a href="${link}">
-        <h2 class="card__name">${item.Name}</h2>
-      </a>
-      <p class="cart-card__color">${item.Colors?.[0]?.ColorName || ""}</p>
-      <p class="cart-card__quantity">qty: 1</p>
-      <p class="cart-card__price">$${item.FinalPrice}</p>
-    </li>
-  `;
+  return `<li class="cart-card divider">
+    <a href="/product_pages/index.html?product=${item.Id}" class="cart-card__image">
+      <img src="${item.Image}" alt="${item.Name}" />
+    </a>
+    <a href="/product_pages/index.html?product=${item.Id}">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+  </li>`;
 }
+
 
 renderCartContents();
