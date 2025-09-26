b3fa2a99a3f7232ba91a77bc603e1a28dc6dfab5
rcari013 <carinorome@gmail.com>
2025-09-26 15:11:16 +0800

    Added Week 4 feature updates

--- src/js/cart.js
+++ src/js/cart.js
@@ -7,3 +7,11 @@ loadHeaderFooter();
 const listElement = document.querySelector(".product-list");
 const cart = new ShoppingCart("so-cart", listElement);
 cart.init();
+
+// Handle checkout button click
+const checkoutBtn = document.getElementById("checkoutBtn");
+if (checkoutBtn) {
+  checkoutBtn.addEventListener("click", () => {
+    window.location.href = "../checkout/index.html";
+  });
+}
